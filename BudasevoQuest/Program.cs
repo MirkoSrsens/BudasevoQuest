2c01b7a711307bf01df0dcf0c8ad4f75924e3337
MirkoSrsens <37936586+MirkoSrsens@users.noreply.github.com>
2018-12-13 17:19:15 +0100

    as
    as

---
+++ BudasevoQuest/Program.cs
@@ -0,0 +1,20 @@
+﻿using InputManager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BudasevoQuest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadKey();
+
+            var inputData = new InputData();
+        }
+    }
+}
